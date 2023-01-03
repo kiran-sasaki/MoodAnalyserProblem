5953ff1ca0857a18f385d2f572657c41ccea19b9
kiran-sasaki <kiransekar1111@gmail.com>
2023-01-03 17:00:24 +0530

    [Kiran] Add: Welcome Message

---
+++ MoodAnalyserProbelm/MoodAnalyser.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyserProbelm
+{
+    public class MoodAnalyser
+    {
+        public void AnalyseMood()
+        {
+            Console.WriteLine("Welcome to Mood Analyser Problem");
+        }
+    }
+}
